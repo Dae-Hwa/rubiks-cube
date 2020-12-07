a87fe3a2afd67b9e42e590aa7b265df27d906d4c
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-12-07 23:03:44 +0900

    Refactor WordPusher
    왼쪽으로 이동을 음수로 표현하던 것을 DIRECTION_LEFT, DIRECTION_RIGHT 에 따라 판단하여 단어를 밀어내도록 수정

--- src/com/codesquad/rubiks_cube/wordpusher/WordPusher.java
+++ src/com/codesquad/rubiks_cube/wordpusher/WordPusher.java
@@ -3,8 +3,12 @@ package com.codesquad.rubiks_cube.wordpusher;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
+import java.util.List;
 
 public class WordPusher {
+    private static final String DIRECTION_LEFT = "L";
+    private static final String DIRECTION_RIGHT = "R";
+
     private Deque<String> words;
     private int repeatCount;
     private String direction;
@@ -17,26 +21,43 @@ public class WordPusher {
 
     public static WordPusher create(String input) {
         String[] inputs = input.split(" ");
+
+        List<String> words = Arrays.asList(inputs[0].split(""));
+        int repeatCount = Integer.parseInt(inputs[1]);
+        String direction = inputs[2].toUpperCase();
+
+        if (repeatCount < 0) {
+            repeatCount = -repeatCount;
+            direction = getReversedDirection(direction);
+        }
+
         return new WordPusher(
-                new ArrayDeque<>(Arrays.asList(inputs[0].split(""))),
-                Integer.parseInt(inputs[1]),
-                inputs[2]
+                new ArrayDeque<>(words),
+                repeatCount,
+                direction
         );
     }
 
+    private static String getReversedDirection(String direction) {
+        if (direction.equals(DIRECTION_LEFT)) {
+            return DIRECTION_RIGHT;
+        }
+        return DIRECTION_LEFT;
+    }
+
+
     public WordPusher push() {
-        if (direction.toUpperCase().equals("L")) {
-            repeatCount = -repeatCount;
+        if (direction.toUpperCase().equals(DIRECTION_LEFT)) {
+            pushLeft(repeatCount);
+            return this;
         }
 
-        pushLeft(repeatCount);
         pushRight(repeatCount);
-
         return this;
     }
 
     private void pushLeft(int repeatCount) {
-        for (int i = 0; repeatCount < i; i--) {
+        for (int i = 0; i < repeatCount; i++) {
             words.offerLast(words.pollFirst());
         }
     }
