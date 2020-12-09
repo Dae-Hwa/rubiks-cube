8d29461fd2190fe9c869f0a46a781133d691e830
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-12-10 00:16:55 +0900

    Update Main
    - 명령어 입력 및 실행 추가
    - README.md에 완료사항 반영

--- src/com/codesquad/rubiks_cube/flatcube/Main.java
+++ src/com/codesquad/rubiks_cube/flatcube/Main.java
@@ -1,12 +1,50 @@
 package com.codesquad.rubiks_cube.flatcube;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
         String[] blocks = new String[]{"R", "R", "W", "W", "B", "B", "G", "G"};
         String mainBlock = "C";
         FlatCube flatCube = new FlatCube(blocks, mainBlock);
 
-        System.out.println(flatCube.toString());
-        System.out.print("CUBE> ");
+        System.out.print(flatCube.toString());
+
+        boolean isEnd = false;
+
+        while (!isEnd) {
+            System.out.println();
+            System.out.print("CUBE> ");
+
+            isEnd = executeCommands(flatCube, getInput());
+        }
+
+        System.out.println("Bye~");
+    }
+
+    public static boolean executeCommands(FlatCube flatCube, String commands) {
+        for (String command : commands.split("")) {
+            if (command.equals("Q")) {
+                return true;
+            }
+
+            System.out.println();
+            System.out.println(command);
+            System.out.print(flatCube.push(command).toString());
+        }
+
+        return false;
+    }
+
+    public static String getInput() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
