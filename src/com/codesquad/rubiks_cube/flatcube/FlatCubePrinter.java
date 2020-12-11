7b0916e149e74460636f9dc5ca6f51d014283041
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-12-12 08:30:43 +0900

    Add overload methods of FlatCubePrinter.printFlatCube with getPrintableFlatCube

--- src/com/codesquad/rubiks_cube/flatcube/FlatCubePrinter.java
+++ src/com/codesquad/rubiks_cube/flatcube/FlatCubePrinter.java
@@ -1,20 +1,49 @@
 package com.codesquad.rubiks_cube.flatcube;
 
+import java.util.List;
+
 public class FlatCubePrinter {
-    public static void printFlatCube(FlatCubeDTO flatCubeDTO) {
+    private static String getPrintableFlatCube(FlatCubeDTO flatCubeDTO, String prefix, String postfix) {
         StringBuilder sb = new StringBuilder();
 
         String[][] cube = flatCubeDTO.toArray();
 
         for (String[] row : cube) {
+            sb.append(prefix);
             for (String column : row) {
                 sb.append(column)
                         .append(" ");
             }
+            sb.append(postfix)
+                    .append(System.lineSeparator());
+        }
+
+        return sb.toString();
+    }
+
+    public static void printFlatCube(FlatCubeDTO flatCubeDTO) {
+        printFlatCube(flatCubeDTO, "", "");
+    }
+
+    public static void printFlatCube(FlatCubeDTO flatCubeDTO, String prefix, String postfix) {
+        System.out.print(getPrintableFlatCube(flatCubeDTO, prefix, postfix));
+    }
+
+    public static void printFlatCubes(List<FlatCubeDTO> flatCubeDTOs) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3 * 4; j++) {
+                String current = flatCubeDTOs.get(j / 3).toArray()[i][j % 3];
+                sb.append(current)
+                        .append(" ");
+                if (j % 3 == 2) {
+                    sb.append("     ");
+                }
+            }
             sb.append(System.lineSeparator());
         }
 
-        System.out.print(sb.toString());
+        System.out.println(sb.toString());
     }
 
     public static void printPrompt() {
