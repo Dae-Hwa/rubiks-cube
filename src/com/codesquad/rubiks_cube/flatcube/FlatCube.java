1d99023f8da18920a43e442322c5aff097a1b92a
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-12-12 02:40:37 +0900

    Create FlatCubeDTO and update FlatCube
    - FlatCube의 DTO 생성
    - toArray 메소드 DTO로 이동 및 toDTO 메소드 생성

--- src/com/codesquad/rubiks_cube/flatcube/FlatCube.java
+++ src/com/codesquad/rubiks_cube/flatcube/FlatCube.java
@@ -8,7 +8,7 @@ import com.codesquad.rubiks_cube.wordpusher.WordPusherDTO;
 import java.util.*;
 
 public class FlatCube {
-    private static final int CUBE_SIZE = 3;
+    public static final int CUBE_SIZE = 3;
     /**
      * 큐브를 달팽이집 형식으로 표현
      * [blocks[0]][blocks[1]][blocks[2]]
@@ -115,49 +115,15 @@ public class FlatCube {
         }
     }
 
-    /**
-     * 일차원으로 늘어트려져있는 큐브를 2차원으로 변경시킨다.
-     *
-     * @return 아래와 같은 형태가 된다. <br/>
-     * [blocks[0]][blocks[1]][blocks[2]]<br/>
-     * [blocks[7]][mainBlock][blocks[3]]<br/>
-     * [blocks[6]][blocks[5]][blocks[4]]
-     */
-    public String[][] toArray() {
-        String[][] blocks = new String[CUBE_SIZE][CUBE_SIZE];
-
-        // 부호에 따라 음의 방향, 양의 방향
-        int direction = 1;
-        int row = 0;
-        int column = -1;
-
-        int range = CUBE_SIZE;
-        for (int i = -1; i < this.blocks.length - 1; direction = -direction) {
-            // 가로줄 채우기
-            for (int j = 0; j < range; j++) {
-                i++;
-                column += direction;
-                blocks[row][column] = this.blocks[i];
-            }
-            --range;
-            // 세로줄 채우기
-            for (int j = 0; j < range; j++) {
-                i++;
-                row += direction;
-                blocks[row][column] = this.blocks[i];
-            }
-        }
-
-        blocks[CUBE_SIZE / 2][CUBE_SIZE / 2] = mainBlock;
-
-        return blocks;
+    public FlatCubeDTO toDTO() {
+        return new FlatCubeDTO(blocks.clone(), mainBlock);
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
 
-        String[][] cube = toArray();
+        String[][] cube = toDTO().toArray();
 
         for (String[] row : cube) {
             for (String column : row) {
---
+++ src/com/codesquad/rubiks_cube/flatcube/FlatCubeDTO.java
@@ -0,0 +1,49 @@
+package com.codesquad.rubiks_cube.flatcube;
+
+public class FlatCubeDTO {
+    private final String[] blocks;
+    private final String mainBlock;
+
+    public FlatCubeDTO(String[] blocks, String mainBlock) {
+        this.blocks = blocks;
+        this.mainBlock = mainBlock;
+    }
+
+    /**
+     * 일차원으로 늘어트려져있는 큐브를 2차원으로 변경시킨다.
+     *
+     * @return 아래와 같은 형태가 된다. <br/>
+     * [blocks[0]][blocks[1]][blocks[2]]<br/>
+     * [blocks[7]][mainBlock][blocks[3]]<br/>
+     * [blocks[6]][blocks[5]][blocks[4]]
+     */
+    public String[][] toArray() {
+        String[][] blocks = new String[FlatCube.CUBE_SIZE][FlatCube.CUBE_SIZE];
+
+        // 부호에 따라 음의 방향, 양의 방향
+        int direction = 1;
+        int row = 0;
+        int column = -1;
+
+        int range = FlatCube.CUBE_SIZE;
+        for (int i = -1; i < this.blocks.length - 1; direction = -direction) {
+            // 가로줄 채우기
+            for (int j = 0; j < range; j++) {
+                i++;
+                column += direction;
+                blocks[row][column] = this.blocks[i];
+            }
+            --range;
+            // 세로줄 채우기
+            for (int j = 0; j < range; j++) {
+                i++;
+                row += direction;
+                blocks[row][column] = this.blocks[i];
+            }
+        }
+
+        blocks[FlatCube.CUBE_SIZE / 2][FlatCube.CUBE_SIZE / 2] = mainBlock;
+
+        return blocks;
+    }
+}
