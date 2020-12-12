f2d3cc601aa44f45cf0617b56cafa8a16a8f4665
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-12-12 20:43:15 +0900

    Add method use BlockPosition for get index at LinkedCube

--- src/com/codesquad/rubiks_cube/rubikscube/LinkedCube.java
+++ src/com/codesquad/rubiks_cube/rubikscube/LinkedCube.java
@@ -1,5 +1,6 @@
 package com.codesquad.rubiks_cube.rubikscube;
 
+import com.codesquad.rubiks_cube.flatcube.BlockPosition;
 import com.codesquad.rubiks_cube.flatcube.FlatCube;
 
 public class LinkedCube {
@@ -11,13 +12,20 @@ public class LinkedCube {
         this.startPosition = startPosition;
     }
 
-    // 여기서 블록 집어넣기
     public String[] getBlocksStartAt(int index) {
         return flatCube.getBlocksStartAt(index + startPosition);
+    }
+
+    public String[] getBlocksStartAt(BlockPosition blockPosition) {
+        return getBlocksStartAt(blockPosition.VALUE);
     }
 
     public LinkedCube setBlocksStartAt(int index, String[] blocks) {
         flatCube.setBlocksStartAt(index + startPosition, blocks);
         return this;
+    }
+
+    public LinkedCube setBlocksStartAt(BlockPosition blockPosition, String[] blocks) {
+        return setBlocksStartAt(blockPosition.VALUE, blocks);
     }
 }
